8cbdd4cdf6d8260af5664c63af22f4fdef6cc545
ZimboPro <dejagerleslie@gmail.com>
2019-01-23 16:37:45 +0200

    catching getopt error

--- image_to_cbr/creator.py
+++ image_to_cbr/creator.py
@@ -5,21 +5,6 @@ import os
 import sys
 import getopt
 
-def get_all_file_paths(directory): 
-  
-    # initializing empty file paths list 
-    file_paths = [] 
-  
-    # crawling through directory and subdirectories 
-    for root, directories, files in os.walk(directory): 
-        for filename in files: 
-            # join the two strings in order to form the full filepath. 
-            filepath = os.path.join(root, filename) 
-            file_paths.append(filepath) 
-  
-    # returning all file paths 
-    return file_paths    
-
 def rename_file(filename, root):
     temp = os.path.splitext(filename)[0].zfill(3) + os.path.splitext(filename)[1]
     temp = os.path.join(root, temp) 
