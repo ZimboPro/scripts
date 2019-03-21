85a636783cb96d9ed0e2c1ec6d2779cceb102fd5
ZimboPro <dejagerleslie@gmail.com>
2019-03-21 17:38:59 +0200

    renaming files

---
+++ rename/README.md
@@ -0,0 +1,30 @@
+# Renaming files and/or folders bying deleting parts of the name or replace a part of the name
+## How it works
+
+The script takes in the flags and searches the current directory and finds names that contain the
+specified string and the either deletes or replaces that part of the name.
+
+### Delete option
+
+eg `python3 creator.py -d "es"` 
+or 
+`python3 creator.py --delete "es"` 
+
+This will delete the first "es" in the name
+
+test > tt
+testTest > ttTest
+
+### Replace option
+
+This option works with the delete option
+
+eg `python3 creator.py -d "es" -r "oo"` 
+or 
+`python3 creator.py -d "es" --replace "oo"` 
+
+test > toot
+testTest > tootTest
+
+## NOTE
+This script requires python3
---
+++ rename/rename_files.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+import os
+import sys
+import getopt
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'd:r:', ['delete=', 'replace='])
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+
+removeStr = ""
+replaceStr = ""
+flags = 0
+
+def usage():
+    print("python3 rename_files.py -d \"string\" // python3 rename_files.py --delete \"string\"")
+    print("OR")
+    print("python3 rename_files.py -r \"string\" // python3 rename_files.py --replace \"string\"")
+    print("NOTE: The --replace/-r flag needs to be used in junction with the --delete/-d flag")
+
+def setReplace(arg):
+    global replaceStr
+    global flags
+    flags = flags + 2
+    replaceStr = arg
+
+def setRemove(arg):
+    global removeStr
+    global flags
+    flags = flags + 1
+    removeStr = arg
+
+def getFiles():
+    return os.listdir(os.curdir)
+
+def removeString():
+    global removeStr
+    files = getFiles();
+    for f in files:
+        if removeStr in f:
+            newName = f.replace(removeStr, "", 1)
+            os.rename(f, newName)
+
+def replaceString():
+    global replaceStr
+    global removeStr
+    files = getFiles();
+    for f in files:
+        if removeStr in f:
+            newName = f.replace(removeStr, replaceStr, 1)
+            os.rename(f, newName)
+
+def executeFlags():
+    if flags == 1:
+        removeString()
+    elif flags == 3:
+        replaceString()
+    elif flags == 2:
+        print("The --replace/-r flag needs to be used in junction with the --delete/-d flag")
+    else:
+        print("Invalid flags")
+        usage()
+    
+
+def main(argv):
+    if len(argv) > 2:
+        for opt, arg in opts:
+            if opt in ('-d', '--delete'):
+                setRemove(arg)         
+            elif opt in ('-r', '--replace'):
+                setReplace(arg)
+        executeFlags()
+    else:         
+        usage()
+
+if __name__ == "__main__": 
+    main(sys.argv) 
