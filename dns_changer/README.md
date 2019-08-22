e7c5ab6fb69d30671188d75f90aa88ed40cc5da6
ZimboPro <dejagerleslie@gmail.com>
2019-08-22 18:01:40 +0200

    create and add install shell script

--- dns_changer/README.md
+++
@@ -1,9 +0,0 @@
-nameserver 208.67.222.123
-nameserver 208.67.220.123your network to use a specific DNS regardless of network connected.
-## How does it work
-It watch the `/etc/resolv.conf` file on unix systems. Once a change is detected it will force the DNS
-to be set.
-
-## Note
-This script was created on Debain so I am not sure if it will work on a BSD or Mac. Windows was not
-considered during development.    
---
+++ shellScripts/installPrograms.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sudo apt-get update -y
+
+# install basic required programs
+sudo apt install -y vim
+sudo apt install -y htop
+sudo apt install -y curl wget
+
+# git
+sudo apt install -y git
+
+# vs code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update -y
+sudo apt install -y code
+sudo rm -f microsoft.gpg
+
+# python 3
+sudo add-apt-repository ppa:deadsnakes/ppa -y )
+sudo apt install -y python3
+# chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt --fix-broken install -y
+rm -f google-chrome-stable_current_amd64.deb
+
+# sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update -y
+sudo apt install -y sublime-text
+
+# Archive Extractors
+sudo apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller
+
+# Calibre - Ebook reader and converter
+sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+
+# finish
+sudo apt update && sudo apt upgrade -y
+sudo apt --fix-broken install -y
